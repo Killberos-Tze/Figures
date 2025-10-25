75ab4c3d5ef9e0b0702df3252bae44e5ae095ef2
Vladislav Jovanov <vladislav.jovanov@ihtm.bg.ac.rs>
2025-10-25 11:41:42 +0200

    changes to plot multimeter data

--- Figures.py
+++ Figures.py
@@ -10,6 +10,7 @@ from matplotlib.figure import Figure
 from numpy import min as npmin
 from numpy import max as npmax
 from numpy import append as npappend
+from numpy import array as nparray
 
 #figure with of without linked right axes
 class FigureXY2(Figure):
@@ -57,10 +58,23 @@ class FigureXY2(Figure):
         self._axy.set_xlim(0,1)
         self._axy.set_ylim(0,1)
         if self._y2:
-            self._axy.plot([],[],color='tab:blue')
-            self._axy2.plot([],[],color='tab:red')
             self._axy2.set_ylim(0,1)
 
+    def _init_plots(self):
+        if self._y2:
+            if len(self._axy.lines)==0:
+                self._axy.plot([],[],color='tab:blue')
+            if len(self._axy2.lines)==0:
+                self._axy2.plot([],[],color='tab:red')
+        else:
+            if len(self._axy.lines)==0:
+                self._axy.plot([],[])
+    def _clear_plots(self):
+        while (len(self._axy.lines)):
+            self._axy.lines[-1].remove()
+        if self._y2:
+            while (len(self._axy2.lines)):
+                self._axy2.lines[-1].remove()
     #if x negative i start from 1.02xmin, if x positive I start from 0
     #0.98 is here if I don't want to use 0 as reference
     def _find_min(self,x):
@@ -73,18 +87,80 @@ class FigureXY2(Figure):
     #not private
     #plots the data it receives (appending should be done in main if needed)
     def plot_data(self,x,y,y2=None):#x, y, y2 are just numpy arrays
+        self._init_plots()
         if len(x)==len(y):
             self._axy.set_xlim(self._find_min(x),self._find_max(x))
             self._axy.set_ylim(self._find_min(y),self._find_max(y))
-        if len(self._axy.lines)!=0:
             self._axy.lines[-1].set_xdata(x)
             self._axy.lines[-1].set_ydata(y)
-        if self._y2:
-            if len(x)==len(y2):
-                self._axy2.set_ylim(self._find_min(y2),self._find_max(y2))
+            if self._y2:
+                if len(x)==len(y2):
+                    self._axy2.set_ylim(self._find_min(y2),self._find_max(y2))
+            self._axy2.lines[-1].set_ydata(y2)
+        self.canvasdraw()
 
+#appends and then plots data
+    def append_plot_data(self,x,y,y2=None):
+        if len(self._axy.lines)!=0:
+            xold=self._axy.lines[-1].get_xdata()
+            yold=self._axy.lines[-1].get_ydata()
+        else:
+            xold=nparray([])
+            yold=nparray([])
+        x=npappend(xold,x)
+        y=npappend(yold,y)
+        if self._y2:
             if len(self._axy2.lines)!=0:
-                self._axy2.lines[-1].set_ydata(y2)
+                y2old=self._axy2.lines[-1].get_ydata()
+            else:
+                y2old=nparray([])
+            y2=npappend(y2old,y2)
+            self.plot_data(x,y,y2)
+        else:
+            self.plot_data(x,y)
+        self.canvasdraw()
+
+    #clears the whole graph
+    def clear_graph(self):
+        self._clear_plots()
+        self._init_figure()
+        self.canvasdraw()
+
+    #clears the last point
+    def delete_last_point(self):
+        if len(self._axy.lines)!=0:
+            x=self._axy.lines[-1].get_xdata()
+            y=self._axy.lines[-1].get_ydata()
+            self._axy.lines[-1].set_xdata(x[:-1])
+            self._axy.lines[-1].set_ydata(y[:-1])
+        if self._y2:
+            if len(self._axy2.lines)!=0:
+                y2=self._axy2.lines[-1].get_ydata()
+                self._axy2.lines[-1].set_ydata(y2[:-1])
+        self.canvasdraw()
+
+    #updates labels
+    def update_labels(self,*args,**kwargs):
+        if args:
+            if len(args)>=3 and self._y2:
+                self._update_y2_label(args[2])
+            if len(args)>=2:
+                self._update_y_label(args[1])
+            if len(args)>=1:
+                self._update_x_label(args[0])
+            return
+        if kwargs:
+            if 'x' in kwargs:
+                self._update_x_label(kwargs['x'])
+            if 'y1' in kwargs:
+                self._update_y_label(kwargs['y1'])
+            if 'y2' in kwargs and self._y2:
+                self._update_y2_label(kwargs['y2'])
+            return
+        self.canvasdraw()
+
+    def set_x_grid_lines(self,num):
+        self._axy.locator_params(axis='x', nbins=num)
         self.canvasdraw()
 
     def clear_xy_curves(self):
@@ -168,69 +244,7 @@ class FigureXY2(Figure):
 
 #multpiple legends add_artist (old legend)
 
-    #appends and then plots data
-    def append_plot_data(self,x,y,y2=None):
-        if len(self._axy.lines)!=0:
-            xold=self._axy.lines[-1].get_xdata()
-            yold=self._axy.lines[-1].get_ydata()
-        x=npappend(xold,x)
-        y=npappend(yold,y)
-        if self._y2:
-            if len(self._axy2.lines)!=0:
-                y2old=self._axy2.lines[-1].get_ydata()
 
-            y2=npappend(y2old,y2)
-            self.plot_data(x,y,y2)
-        else:
-            self.plot_data(x,y)
-
-        self.canvasdraw()
-    #clears the whole graph
-    def clear_graph(self):
-        while (len(self._axy.lines)):
-            self._axy.lines[-1].remove()
-        if self._y2:
-            while (len(self._axy2.lines)):
-                self._axy2.lines[-1].remove()
-        self._init_figure()
-        self.canvasdraw()
-
-    #clears the last point
-    def delete_last_point(self):
-        if len(self._axy.lines)!=0:
-            x=self._axy.lines[-1].get_xdata()
-            y=self._axy.lines[-1].get_ydata()
-            self._axy.lines[-1].set_xdata(x[:-1])
-            self._axy.lines[-1].set_ydata(y[:-1])
-        if self._y2:
-            if len(self._axy2.lines)!=0:
-                y2=self._axy2.lines[-1].get_ydata()
-                self._axy2.lines[-1].set_ydata(y2[:-1])
-        self.canvasdraw()
-
-    #updates labels
-    def update_labels(self,*args,**kwargs):
-        if args:
-            if len(args)>=3 and self._y2:
-                self._update_y2_label(args[2])
-            if len(args)>=2:
-                self._update_y_label(args[1])
-            if len(args)>=1:
-                self._update_x_label(args[0])
-            return
-        if kwargs:
-            if 'x' in kwargs:
-                self._update_x_label(kwargs['x'])
-            if 'y1' in kwargs:
-                self._update_y_label(kwargs['y1'])
-            if 'y2' in kwargs and self._y2:
-                self._update_y2_label(kwargs['y2'])
-            return
-        self.canvasdraw()
-
-    def set_x_grid_lines(self,num):
-        self._axy.locator_params(axis='x', nbins=num)
-        self.canvasdraw()
 
 class FigureLineMap(Figure):
     def __init__(self,*args,figsize=(9.5/2.54,14/2.54),**kwargs):
